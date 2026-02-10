48c367d011b6d4b57401122d2c9423207fcacb95
Amgalanbayar Amgalanbaatar <amgalanbayar@Amgalanbayars-MacBook-Air.local>
2026-02-10 17:56:08 +0800

    trusted origins

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,6 +3,8 @@ import { pool } from "@/lib/db";
 
 export const auth = betterAuth({
   database: pool,
+  baseURL: process.env.BETTER_AUTH_URL,
+  trustedOrigins: [process.env.BETTER_AUTH_URL!],
   emailAndPassword: {
     enabled: true,
   },
